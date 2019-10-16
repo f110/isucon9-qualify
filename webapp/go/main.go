8501751debf58babb062f6ff3009bbf102da6ff1
Fumihiro Itoh <fmhrit@gmail.com>
2019-10-17 03:10:53 +0900

    categoriesの参照が残ってた

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -2183,14 +2183,6 @@ func getSettings(w http.ResponseWriter, r *http.Request) {
 
 	ress.PaymentServiceURL = getPaymentServiceURL()
 
-	categories := []Category{}
-
-	err := dbx.Select(&categories, "SELECT * FROM `categories`")
-	if err != nil {
-		log.Print(err)
-		outputErrorMsg(w, http.StatusInternalServerError, "db error")
-		return
-	}
 	ress.Categories = categories
 
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
